c2e9fd60c1a6f3eaa0576efdc6a2d53207201e61
NHAT NGUYEN <nguye101@unlv.nevada.edu>
2025-09-06 03:47:51 +0000

    My solution for Project 1 on xv6

---
+++ user/hello.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main()
+{
+    printf("Hello world, this is Nhat Nguyen.\n");
+    printf("I am in CS 370, Fall 2025, section 1001.\n\n");
+    exit(0);
+}
